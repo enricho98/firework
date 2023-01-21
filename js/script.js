c8c444e8ccaae7317e1b581322f4b15b16218738
enricho98 <enrichoshen98@gmail.com>
2023-01-22 03:36:23 +0700

    nice

--- js/script.js
+++ js/script.js
@@ -2301,7 +2301,7 @@ if (IS_HEADER) {
 	init();
 } else {
 	// Allow status to render, then preload assets and start app.
-	setLoadingStatus('正在点燃导火线');
+	setLoadingStatus('Hehehe by Me');
 	setTimeout(() => {
 		soundManager.preload()
 		.then(
